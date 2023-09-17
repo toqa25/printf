df15048288966789a377872667b62a7f0d310163
Areejslama <Areejslama@github.com>
2023-09-17 16:42:25 -0700

    printf.c

--- _printf.c
+++ _printf.c
@@ -8,54 +8,54 @@
 
 int _printf(const char *format, ...)
 {
-	va_list argu;
-	va_start(argu, format);
+va_list argu;
+va_start(argu, format);
 
-	int char_no = 0;
+int char_no = 0;
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')
-			if (*format == '%')
-			{
-				putchar('%');
-				char_no++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(argu int);
-				putchar(c);
-				char_no++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(argu, char *);
-				if (str != NULL)
-				{
-					while (*str) 
-					{
-						putchar(*str);
-						str++;
-						char_no++;
-					}
-				}
-			}
-			else if (*format == 'i' || *format == 'd') 
-			{
-				int num = va_arg(argu, int);
-				printf("%d", num);
-				char_no += snprintf(NULL, 0, "%d", num);
-			}
-			else
-		       	{
-		putchar(*format);
-		char_no++;
-	}
-		format++;
-	}
-	va_end(argu);
-	return (char_no);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == '\0')
+if (*format == '%')
+{
+putchar('%');
+char_no++;
+}
+else if (*format == 'c')
+{
+char c = va_arg(argu int);
+putchar(c);
+char_no++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(argu, char *);
+if (str != NULL)
+{
+while (*str)
+{
+putchar(*str);
+str++;
+char_no++;
+}
+}
+}
+else if (*format == 'i' || *format == 'd')
+{
+int num = va_arg(argu, int);
+printf("%d", num);
+char_no += snprintf(NULL, 0, "%d", num);
+}
+else
+{
+putchar(*format);
+char_no++;
+}
+format++;
+}
+va_end(argu);
+return (char_no);
 }
