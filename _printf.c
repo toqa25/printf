e42aeefc3e924eb63499070f8fdf5e3f033ac781
Toqa <toqa.ashraf235@gmail.com>
2023-09-18 10:51:13 -0700

    printf.c

--- _printf.c
+++ _printf.c
@@ -7,7 +7,7 @@
 int _printf(const char *format, ...)
 {va_list argu;
 va_start(argu, format);
-int char_no = 0;
+int car_no = 0;
 
 if (format == NULL)
 {
@@ -19,22 +19,22 @@ if (*format == '%')
 {format++;
 if (*format == '%')
 {putchar('%');
-char_no++; }
+car_no++; }
 else if (*format == 'c')
 {char c = va_arg(argu, int);
 putchar(c);
-char_no++; }
+car_no++; }
 else if (*format == 's')
 {char *s = va_arg(argu, char *);
 while (*s)
 {putchar(*s);
 s++;
-char_no++;
+car_no++;
 }}
 else
 {putchar(*format);
-char_no++; }
+car_no++; }
 format++; }
 va_end(argu);
-return (char_no);
+return (car_no);
 }
