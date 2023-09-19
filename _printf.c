1e4a8821b59f52e546d079b15bf05023d3533dfa
Areejslama <Areejslama@github.com>
2023-09-19 12:19:08 -0700

    printf.c

--- _printf.c
+++ _printf.c
@@ -6,34 +6,35 @@
  * Return: printed char
  */
 int _printf(const char *format, ...)
+{va_list argu;
+int car_no = 0;
+va_start(argu, format);
+if (format == NULL)
 {
-	int p = 0;
-
-	va_list argu;
-
-		if (format == NULL)
-		{
-			return (-1);
-		}
-		va_start(argu, format);
-		while (*format)
-		{
-			if (*format == '%')
-			{
-				format++;
-			}
-		else
-		{
-			_putchar(*format + '0');
-			p++;
-		}
-		if (*format == '\0')
-		{
-			break;
-		}
-		p += 2;
-		}
-		p++;
-	va_end(argu);
-	return (p);
+	return (-1);
+}
+while (*format)
+{
+if (*format == '%')
+{format++;
+if (*format == '%')
+{putchar('%');
+car_no++; }
+else if (*format == 'c')
+{char c = va_arg(argu, int);
+putchar(c);
+car_no++; }
+else if (*format == 's')
+{char *s = va_arg(argu, char *);
+while (*s)
+{putchar(*s);
+s++;
+car_no++;
+}}
+else
+{putchar(*format);
+car_no++; }
+format++; }
+va_end(argu);
+return (car_no);
 }
