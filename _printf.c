dbd7698eb6ecc38e9129e5bde37fce53c492249c
Toqa <toqa.ashraf235@gmail.com>
2023-09-19 11:00:13 -0700

    printf.c

--- _printf.c
+++ _printf.c
@@ -1,40 +1,33 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - print variables
+ * @format:char
+ * Return: printed char
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-	};
+	int p = 0;
 
 	va_list args;
-	int a = 0, b, len = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-Here:
-	while (format[a] != '\0')
+
+	while (*format != '\0')
 	{
-		b = 13;
-		while (b > = 0)
+		if (*format == '%')
 		{
-			if (m[b].id[0] == format[a] && m[b].id[1] == format[a + 1])
-			{
-				len += m[b].f(args);
-				a = a + 2;
-				goto Here;
-			}
-			b--;
+			format++;
+			p = write(format, args, p);
+			format++;
 		}
-		_putchar(format[a]);
-		len++;
-		a++;
+		else
+		{
+			_putchar(*format);
+			p++;
+			format++;
+		}
 	}
 	va_end(args);
-	return (len);
+	return (p);
 }
